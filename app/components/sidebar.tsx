7ed2d837703ddfb45578531b485dbc69b32d5c24
FROGYu <129513910+FROGYu@users.noreply.github.com>
2023-06-13 19:26:48 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -119,9 +119,10 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>ChatGPT For Friends</div>
         <div className={styles["sidebar-sub-title"]}>
           Build your own AI assistant.
+          Technology changes the World
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
